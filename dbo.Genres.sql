694371f0d42fff34050f4978e18e5d536939734e
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 11:38:46 -0400

    Create Genres Table

---
+++ dbo.Genres.sql
@@ -0,0 +1,6 @@
+﻿
+CREATE TABLE [dbo].[Genres] (
+    [GenreID] INT           IDENTITY (1, 1) NOT NULL,
+    [Genres]  NVARCHAR (50) NOT NULL,
+	CONSTRAINT [PK_GenreID] PRIMARY KEY CLUSTERED ([GenreID] ASC)
+);
