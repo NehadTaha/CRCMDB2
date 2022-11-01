d93d68738f227e58bfc21e0e63ec13cc0db2408b
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 13:00:44 -0400

    Create MovieGenre Table

---
+++ dbo.MovieGenre table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MovieGenre] (
+    [MovieGenre] INT IDENTITY (1, 1) NOT NULL,
+    [GenreId]    INT NOT NULL,
+    [MovieId]    INT NOT NULL
+	CONSTRAINT [PK_MovieGenre] PRIMARY KEY CLUSTERED ([MovieGenre] ASC)
+	FOREIGN KEY (MovieId) REFERENCES Movies(MovieId),
+     FOREIGN KEY (GenreId) REFERENCES Genres(GenreId)
+);
