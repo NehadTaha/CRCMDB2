7539fda71a93f38790a6cd15fd8e5e43721f61c7
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 13:33:13 -0400

    Code close Button

--- CRCCMDB2/ActorsForm.cs
+++ CRCCMDB2/ActorsForm.cs
@@ -16,5 +16,25 @@ namespace CRCCMDB2
         {
             InitializeComponent();
         }
+
+        private void actorsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.actorsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.lab4DataBaseDataSet);
+
+        }
+
+        private void ActorsForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'lab4DataBaseDataSet.Actors' table. You can move, or remove it, as needed.
+            this.actorsTableAdapter.Fill(this.lab4DataBaseDataSet.Actors);
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
