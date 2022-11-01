e84eac166aff15d5b951f420ac7bed645ab51cbd
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 11:54:12 -0400

    Create Actors table

---
+++ dbo.Actors.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Actors] (
+    [ActorID]   INT       NOT NULL,
+    [Name]      VARCHAR (50) NOT NULL,
+    [BirthYear] INT    IDENTITY (1, 1) NOT NULL, 
+    [DeathYear] VARCHAR (50) NOT NULL,
+   
+    [Played_in] VARCHAR (50) NOT NULL
+	CONSTRAINT [PK_ActorID] PRIMARY KEY CLUSTERED ([ActorID] ASC)
+	
+);
