9324acf380a5e35c4b1fc3fabfbf2ea6b5934a1e
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 11:10:30 -0400

    Create Movies table

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Movies]
+(
+	
+    [MovieId] INT          NOT NULL IDENTITY,
+    [Title]   VARCHAR (50) NOT NULL,
+    [Year]    INT          NOT NULL,
+	[Genre]     VARCHAR (50)          NOT NULL,
+    CONSTRAINT [PK_Movies] PRIMARY KEY CLUSTERED ([MovieId] ASC)
+
+)
