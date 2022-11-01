dc36a55f243137d9bb9485121e8ecf816aaad02d
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 13:06:30 -0400

    Insert MovieGenre data

---
+++ dbo.MovieGenre(2).data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[MovieGenre] ON
+INSERT INTO [dbo].[MovieGenre] ([MovieGenre], [GenreId], [MovieId]) VALUES (1, 1, 1)
+INSERT INTO [dbo].[MovieGenre] ([MovieGenre], [GenreId], [MovieId]) VALUES (2, 1, 3)
+INSERT INTO [dbo].[MovieGenre] ([MovieGenre], [GenreId], [MovieId]) VALUES (3, 8, 2)
+INSERT INTO [dbo].[MovieGenre] ([MovieGenre], [GenreId], [MovieId]) VALUES (4, 8, 3)
+INSERT INTO [dbo].[MovieGenre] ([MovieGenre], [GenreId], [MovieId]) VALUES (5, 17, 2)
+INSERT INTO [dbo].[MovieGenre] ([MovieGenre], [GenreId], [MovieId]) VALUES (6, 18, 1)
+SET IDENTITY_INSERT [dbo].[MovieGenre] OFF
