832b1f1fb075b55eeae9171f21cc9948c90fded8
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-01 13:40:58 -0400

    Call the forms from form1

--- CRCCMDB2/Form1.Designer.cs
+++ CRCCMDB2/Form1.Designer.cs
@@ -36,49 +36,58 @@
             // 
             // manageMoviesButton
             // 
-            this.manageMoviesButton.Location = new System.Drawing.Point(76, 47);
+            this.manageMoviesButton.Location = new System.Drawing.Point(57, 38);
+            this.manageMoviesButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.manageMoviesButton.Name = "manageMoviesButton";
-            this.manageMoviesButton.Size = new System.Drawing.Size(206, 64);
+            this.manageMoviesButton.Size = new System.Drawing.Size(154, 52);
             this.manageMoviesButton.TabIndex = 0;
             this.manageMoviesButton.Text = "Manage Movies";
             this.manageMoviesButton.UseVisualStyleBackColor = true;
+            this.manageMoviesButton.Click += new System.EventHandler(this.manageMoviesButton_Click);
             // 
             // manageActorsButton
             // 
-            this.manageActorsButton.Location = new System.Drawing.Point(312, 47);
+            this.manageActorsButton.Location = new System.Drawing.Point(234, 38);
+            this.manageActorsButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.manageActorsButton.Name = "manageActorsButton";
-            this.manageActorsButton.Size = new System.Drawing.Size(190, 64);
+            this.manageActorsButton.Size = new System.Drawing.Size(142, 52);
             this.manageActorsButton.TabIndex = 1;
             this.manageActorsButton.Text = "Manage Actors";
             this.manageActorsButton.UseVisualStyleBackColor = true;
+            this.manageActorsButton.Click += new System.EventHandler(this.manageActorsButton_Click);
             // 
             // manageGenresButton
             // 
-            this.manageGenresButton.Location = new System.Drawing.Point(555, 47);
+            this.manageGenresButton.Location = new System.Drawing.Point(416, 38);
+            this.manageGenresButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.manageGenresButton.Name = "manageGenresButton";
-            this.manageGenresButton.Size = new System.Drawing.Size(196, 73);
+            this.manageGenresButton.Size = new System.Drawing.Size(147, 59);
             this.manageGenresButton.TabIndex = 2;
             this.manageGenresButton.Text = "Manage Genres";
             this.manageGenresButton.UseVisualStyleBackColor = true;
+            this.manageGenresButton.Click += new System.EventHandler(this.manageGenresButton_Click);
             // 
             // closeButton
             // 
-            this.closeButton.Location = new System.Drawing.Point(342, 181);
+            this.closeButton.Location = new System.Drawing.Point(256, 147);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(105, 36);
+            this.closeButton.Size = new System.Drawing.Size(79, 29);
             this.closeButton.TabIndex = 3;
             this.closeButton.Text = "Close";
             this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 257);
+            this.ClientSize = new System.Drawing.Size(600, 191);
             this.Controls.Add(this.closeButton);
             this.Controls.Add(this.manageGenresButton);
             this.Controls.Add(this.manageActorsButton);
             this.Controls.Add(this.manageMoviesButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
